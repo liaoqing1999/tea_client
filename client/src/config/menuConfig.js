585af15d1a7269105d8593333e0e6f2387dce108
廖青1 <qing.liao1@chinacreator.com>
2020-04-28 09:32:38 +0800

    修改后台菜单

--- client/src/config/menuConfig.js
+++ client/src/config/menuConfig.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { TrademarkOutlined, UserOutlined, DesktopOutlined, TeamOutlined, CloudOutlined, ToolOutlined, SolutionOutlined, MessageOutlined, EyeInvisibleOutlined, ExperimentOutlined, TransactionOutlined, BankOutlined } from '@ant-design/icons';
+import {DatabaseOutlined,InsertRowRightOutlined,FileTextOutlined,GoldOutlined, ClusterOutlined,SettingOutlined,TrademarkOutlined,
+    SafetyCertificateOutlined,StockOutlined,AreaChartOutlined, InsertRowLeftOutlined,UserOutlined, DesktopOutlined, TeamOutlined, CloudOutlined, ToolOutlined, 
+    SolutionOutlined,LineChartOutlined,PieChartOutlined,FireOutlined,BarChartOutlined, MessageOutlined, EyeInvisibleOutlined, ExperimentOutlined, TransactionOutlined, BankOutlined } from '@ant-design/icons';
 
 const menuList = [
     {
@@ -29,46 +31,128 @@ const menuList = [
         ]
     },
     {
-        title: '种植阶段',
-        key: '/admin/plant',
-        icon: <CloudOutlined />
+        title: '茶叶产品',
+        key: '/admin/tea',
+        icon: <SafetyCertificateOutlined />,
+        children: [
+            {
+                title: '种植阶段',
+                key: '/admin/plant',
+                icon: <CloudOutlined />
+            },
+            {
+                title: '加工阶段',
+                key: '/admin/process',
+                icon: <ToolOutlined />,
+            },
+            {
+                title: '仓储阶段',
+                key: '/admin/storage',
+                icon: <BankOutlined />,
+            },
+            {
+                title: '检测阶段',
+                key: '/admin/check',
+                icon: <ExperimentOutlined />,
+            },
+            {
+                title: '售卖阶段',
+                key: '/admin/sale',
+                icon: <TransactionOutlined />,
+            },
+        ]
     },
     {
-        title: '加工阶段',
-        key: '/admin/process',
-        icon: <ToolOutlined />,
+        title: '统计分析',
+        key: '/admin/chart',
+        icon: <BarChartOutlined />,
+        children: [
+            {
+                title: '产品分析',
+                key: '/admin/produceChart',
+                icon: <AreaChartOutlined />
+            },
+            {
+                title: '资讯分析',
+                key: '/admin/newsChart',
+                icon: <StockOutlined />
+            },
+            {
+                title: '机构分析',
+                key: '/admin/orgChart',
+                icon: <LineChartOutlined />
+            },
+            {
+                title: '用户分析',
+                key: '/admin/staffChart',
+                icon: <PieChartOutlined />
+            },  
+        ]
     },
     {
-        title: '仓储阶段',
-        key: '/admin/storage',
-        icon: <BankOutlined />,
+        title: '系统管理',
+        key: '/admin/orgSys',
+        icon: <ClusterOutlined />,
+        children: [
+            {
+                title: '机构信息',
+                key: '/admin/orgInfo',
+                icon: <DesktopOutlined />
+            },
+            {
+                title: '机构用户',
+                key: '/admin/staff',
+                icon: <TeamOutlined />
+            },
+            {
+                title: '机构产品',
+                key: '/admin/produce',
+                icon: <InsertRowLeftOutlined />
+            },
+            {
+                title: '机构资讯',
+                key: '/admin/news',
+                icon: <FireOutlined />
+            },  
+        ]
     },
     {
-        title: '检测阶段',
-        key: '/admin/check',
-        icon: <ExperimentOutlined />,
+        title: '平台管理',
+        key: '/admin/sys',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                title: '机构管理',
+                key: '/admin/org',
+                icon: <GoldOutlined />
+            },
+            {
+                title: '用户管理',
+                key: '/admin/staff',
+                icon: <TeamOutlined />
+            },
+            {
+                title: '角色管理',
+                key: '/admin/role',
+                icon: <TrademarkOutlined />
+            },
+            {
+                title: '产品管理',
+                key: '/admin/produce',
+                icon: <InsertRowRightOutlined />
+            },
+            {
+                title: '字典管理',
+                key: '/admin/dict',
+                icon: <DatabaseOutlined />
+            },
+            {
+                title: '资讯管理',
+                key: '/admin/news',
+                icon: <FileTextOutlined />
+            },
+        ]
     },
-    {
-        title: '售卖阶段',
-        key: '/admin/sale',
-        icon: <TransactionOutlined />,
-    },
-    {
-        title: '机构管理',
-        key: '/admin/org',
-        icon: <DesktopOutlined />
-    },
-    {
-        title: '用户管理',
-        key: '/admin/staff',
-        icon: <TeamOutlined />
-    },
-    {
-        title: '角色管理',
-        key: '/admin/role',
-        icon: <TrademarkOutlined />
-    }
-
 ]
 const routeList = [
     '/admin/info',
