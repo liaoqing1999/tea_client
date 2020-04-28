7286fd4f7e8c3c09e137ec320c227428f9af97ce
廖青1 <qing.liao1@chinacreator.com>
2020-04-28 09:29:54 +0800

    添加中文国际化

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from "react";
 //import {Button, message} from 'antd'
-import {BrowserRouter,Route,Switch, Redirect} from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Login from "./pages/login/login";
 import Admin from "./pages/admin/admin";
 import { Trace } from "./pages/trace";
@@ -10,25 +10,29 @@ import Result404 from './components/result/404'
 import Result403 from './components/result/403'
 import Result500 from './components/result/500'
 import "./config/config"
-
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
 /*
 应用的根组件
 */
-export default class App extends Component{
-    render(){
+export default class App extends Component {
+    render() {
         return (
-            <BrowserRouter>
-            <Switch>
-                <Route exact path='/' component={Trace}></Route>
-                <Route exact path='/login' component={Login}></Route>
-                <Route path='/admin' component={Admin}></Route>
-                <Route path='/main' component={Main}></Route>
-                <Route path='/404' component={Result404}></Route>
-                <Route path='/403' component={Result403}></Route>
-                <Route path='/500' component={Result500}></Route>
-                <Redirect to = '/404' /> 
-            </Switch>
-            </BrowserRouter>
+            <ConfigProvider locale={zhCN}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/' component={Trace}></Route>
+                        <Route exact path='/login' component={Login}></Route>
+                        <Route path='/admin' component={Admin}></Route>
+                        <Route path='/main' component={Main}></Route>
+                        <Route path='/404' component={Result404}></Route>
+                        <Route path='/403' component={Result403}></Route>
+                        <Route path='/500' component={Result500}></Route>
+                        <Redirect to='/404' />
+                    </Switch>
+                </BrowserRouter>
+            </ConfigProvider>
+
         )
     }
 }
