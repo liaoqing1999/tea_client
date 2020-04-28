91b641021cd7124eda1418745f00c9c926ae4694
廖青1 <qing.liao1@chinacreator.com>
2020-04-28 09:35:31 +0800

    初始化

--- client/src/pages/admin/storage/index.jsx
+++ client/src/pages/admin/storage/index.jsx
@@ -1,9 +1,28 @@
 import React,{Component} from 'react'
+import memoryUtils from '../../../utils/memoryUtils'
+import { Alert,Tabs  } from 'antd'
+const { TabPane } = Tabs;
 export default class Storage extends Component{
     render(){
-        return(
-            <div className="top">
-                Clause
+        const user = memoryUtils.user
+        return (
+            <div>
+                {user.org ? (
+                    <Tabs defaultActiveKey="1" onChange={this.onChange}>
+                    <TabPane tab="产品" key="1">
+                      Content of Tab Pazne 1
+                    </TabPane>
+                    <TabPane tab="茶叶" key="2">
+                      Content of Tab Pane 2
+                    </TabPane>
+                  </Tabs>
+                ) :
+                    (<Alert
+                        message="警告"
+                        description="您不属于任何机构！."
+                        type="warning"
+                        showIcon
+                    />)}
             </div>
         )
     }
